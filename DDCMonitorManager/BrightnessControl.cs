dc483cbfbbbeb81a74beccf89f80997141954692
Richard Polzin <polzin.richard@gmail.com>
2015-04-23 14:54:01 +0200

    Changed wrapping, basically works now

--- DDCMonitorManager/BrightnessControl.cs
+++ DDCMonitorManager/BrightnessControl.cs
@@ -5,22 +5,94 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Windows.Interop;
 
 namespace DDCMonitorManager
 {
     class BrightnessControl
     {
-        [DllImport("user32.dll")]
-        private static extern Int32 MonitorFromWindow(IntPtr hwnd, Int32 dwflags);
+        [DllImport("user32.dll", EntryPoint = "MonitorFromWindow", SetLastError = true)]
+        public static extern IntPtr MonitorFromWindow(
+            [In] IntPtr hwnd, uint dwFlags);
 
-        [DllImport("Dxva2.dll")]
-        private static extern bool GetNumberOfPhysicalMonitorsFromHMONITOR(Int32 hMonitor, out Int32 num);
+        [DllImport("dxva2.dll", EntryPoint = "GetNumberOfPhysicalMonitorsFromHMONITOR", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetNumberOfPhysicalMonitorsFromHMONITOR(
+            IntPtr hMonitor, ref uint pdwNumberOfPhysicalMonitors);
 
-        [DllImport("Dxva2.dll")]
-        private static extern bool SetMonitorBrightness(Int32 hMonitor, short newBrightness);
+        [DllImport("dxva2.dll", EntryPoint = "GetPhysicalMonitorsFromHMONITOR", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPhysicalMonitorsFromHMONITOR(
+            IntPtr hMonitor,
+            uint dwPhysicalMonitorArraySize,
+            [Out] NativeStructures.PHYSICAL_MONITOR[] pPhysicalMonitorArray);
 
-        [DllImport("Dxva2.dll")]
-        private static extern bool GetMonitorCapabilities(Int32 hMonitor, out Int32 capabilities, out Int32 supportedColorTemperatures);
+        [DllImport("dxva2.dll", EntryPoint = "DestroyPhysicalMonitors", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DestroyPhysicalMonitors(
+            uint dwPhysicalMonitorArraySize, [Out] NativeStructures.PHYSICAL_MONITOR[] pPhysicalMonitorArray);
+
+        [DllImport("dxva2.dll", EntryPoint = "GetMonitorTechnologyType", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetMonitorTechnologyType(
+            IntPtr hMonitor, ref NativeStructures.MC_DISPLAY_TECHNOLOGY_TYPE pdtyDisplayTechnologyType);
+
+        [DllImport("dxva2.dll", EntryPoint = "GetMonitorCapabilities", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetMonitorCapabilities(
+            IntPtr hMonitor, ref uint pdwMonitorCapabilities, ref uint pdwSupportedColorTemperatures);
+
+        [DllImport("dxva2.dll", EntryPoint = "SetMonitorBrightness", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetMonitorBrightness(
+            IntPtr hMonitor, short brightness);
+
+        public class NativeConstants
+        {
+            public const int MONITOR_DEFAULTTOPRIMARY = 1;
+
+            public const int MONITOR_DEFAULTTONEAREST = 2;
+
+            public const int MONITOR_DEFAULTTONULL = 0;
+        }
+
+        public class NativeStructures
+        {
+            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+            public struct PHYSICAL_MONITOR
+            {
+                public IntPtr hPhysicalMonitor;
+
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)] public string szPhysicalMonitorDescription;
+            }
+
+            public enum MC_DISPLAY_TECHNOLOGY_TYPE
+            {
+                MC_SHADOW_MASK_CATHODE_RAY_TUBE,
+
+                MC_APERTURE_GRILL_CATHODE_RAY_TUBE,
+
+                MC_THIN_FILM_TRANSISTOR,
+
+                MC_LIQUID_CRYSTAL_ON_SILICON,
+
+                MC_PLASMA,
+
+                MC_ORGANIC_LIGHT_EMITTING_DIODE,
+
+                MC_ELECTROLUMINESCENT,
+
+                MC_MICROELECTROMECHANICAL,
+
+                MC_FIELD_EMISSION_DEVICE,
+            }
+        }
+
+        private struct PHYSICAL_MONITOR
+        {
+            Int32 hPhysicalMonitor;
+
+        }
 
         private IntPtr hWnd;
 
@@ -31,24 +103,42 @@ namespace DDCMonitorManager
 
         public unsafe bool SetBrightness(short brightness)
         {
-            Int32 hMonitor = MonitorFromWindow(hWnd, 1); //MONITOR_DEFAULTTOPRIMARY
-            Int32 num = 0;
-            GetNumberOfPhysicalMonitorsFromHMONITOR(hMonitor,out num);
-            System.Console.WriteLine(num + " physical monitors detected.");
-            Int32 capabilities = 0, supportedColorTemperatures = 0;
-            if (!GetMonitorCapabilities(hMonitor, out capabilities, out supportedColorTemperatures))
-            { 
-                //DDC/CI not supported
-                System.Console.WriteLine("DDC/CI not supported by monitor.");
-            }
+            IntPtr hMonitor = MonitorFromWindow(hWnd, NativeConstants.MONITOR_DEFAULTTOPRIMARY);
+            int lastWin32Error = Marshal.GetLastWin32Error();
 
-            if (brightness > 255)
-                brightness = 255;
+            uint pdwNumberOfPhysicalMonitors = 0u;
+            bool numberOfPhysicalMonitorsFromHmonitor = GetNumberOfPhysicalMonitorsFromHMONITOR(
+                hMonitor, ref pdwNumberOfPhysicalMonitors);
+            lastWin32Error = Marshal.GetLastWin32Error();
 
-            if (brightness < 0)
-                brightness = 0;
+            NativeStructures.PHYSICAL_MONITOR[] pPhysicalMonitorArray =
+                new NativeStructures.PHYSICAL_MONITOR[pdwNumberOfPhysicalMonitors];
+            bool physicalMonitorsFromHmonitor = GetPhysicalMonitorsFromHMONITOR(
+                hMonitor, pdwNumberOfPhysicalMonitors, pPhysicalMonitorArray);
+            lastWin32Error = Marshal.GetLastWin32Error();
 
-            return SetMonitorBrightness(hMonitor, brightness);
+            uint pdwMonitorCapabilities = 0u;
+            uint pdwSupportedColorTemperatures = 0u;
+            var monitorCapabilities = GetMonitorCapabilities(
+                pPhysicalMonitorArray[0].hPhysicalMonitor, ref pdwMonitorCapabilities, ref pdwSupportedColorTemperatures);
+            lastWin32Error = Marshal.GetLastWin32Error();
+
+            var brightnessWasSet = SetMonitorBrightness(pPhysicalMonitorArray[0].hPhysicalMonitor, (short)brightness);
+            if (brightnessWasSet)
+                System.Console.WriteLine("Brightness set to " + (short)brightness);
+            lastWin32Error = Marshal.GetLastWin32Error();
+
+            NativeStructures.MC_DISPLAY_TECHNOLOGY_TYPE type =
+                NativeStructures.MC_DISPLAY_TECHNOLOGY_TYPE.MC_SHADOW_MASK_CATHODE_RAY_TUBE;
+            var monitorTechnologyType = GetMonitorTechnologyType(
+                pPhysicalMonitorArray[0].hPhysicalMonitor, ref type);
+            lastWin32Error = Marshal.GetLastWin32Error();
+
+            var destroyPhysicalMonitors = DestroyPhysicalMonitors(
+                pdwNumberOfPhysicalMonitors, pPhysicalMonitorArray);
+            lastWin32Error = Marshal.GetLastWin32Error();
+
+            return true;
         }
     }
 }
