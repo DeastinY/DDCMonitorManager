3e946c7321d7b2e3bb20b3d660ede74feb144d50
Richard <polzin.richard@gmail.com>
2015-04-23 10:22:02 +0200

    Added Initial Project
    Slider and first approach, sadly only changing gamma

---
+++ DDCMonitorManager/BrightnessControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DDCMonitorManager
+{
+    class BrightnessControl
+    {
+        [DllImport("gdi32.dll")]
+        private unsafe static extern bool SetDeviceGammaRamp(Int32 hdc, void* ramp);
+
+        public BrightnessControl()
+        {
+        }
+
+        public unsafe bool SetBrightness(short brightness)
+        {
+            Int32 hdc = Graphics.FromHwnd(IntPtr.Zero).GetHdc().ToInt32();
+
+            if (brightness > 255)
+                brightness = 255;
+
+            if (brightness < 0)
+                brightness = 0;
+
+            short* gArray = stackalloc short[3 * 256];
+            short* idx = gArray;
+
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    int arrayVal = i * (brightness + 128);
+
+                    if (arrayVal > 65535)
+                        arrayVal = 65535;
+
+                    *idx = (short)arrayVal;
+                    idx++;
+                }
+            }
+
+            //For some reason, this always returns false?
+            bool retVal = SetDeviceGammaRamp(hdc, gArray);
+
+            //Memory allocated through stackalloc is automatically free'd
+            //by the CLR.
+
+            return retVal;
+        }
+    }
+}
---
+++ DDCMonitorManager/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DDCMonitorManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BrightnessControl brightnessControl;
+        public MainWindow()
+        {
+            InitializeComponent();
+            brightnessControl = new BrightnessControl();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var conv = (short)e.NewValue;
+            if (brightnessControl != null)
+            {
+                brightnessControl.SetBrightness(conv); 
+            }
+        }
+    }
+}
